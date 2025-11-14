d4030f383e4df7ead6ff9fd6024ae42c39737c45
Joshua <joshuatonge1@gmail.com>
2025-11-13 22:28:08 -0800

    outfit page stuff

--- main/main.js
+++ main/main.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const mainPage = document.getElementById("main-page");
   const weatherPage = document.getElementById("weather-report-page");
   const addClothingPage = document.getElementById("add-clothing-page");
+  const closet = document.getElementById("closet");
 
   // Buttons
   const leftButton = document.querySelector(".top-bar #weather-scroll-left");
   const rightButton = document.querySelector(".top-bar #weather-scroll-right");
   const back1 = document.getElementById("back-to-main-1");
   const back2 = document.getElementById("back-to-main-2");
-  const image_input = document.querySelector("#image_input");
 
   // Go to weather report
   leftButton.addEventListener("click", () => {
@@ -33,13 +33,4 @@ document.addEventListener("DOMContentLoaded", () => {
     mainPage.style.display = "flex";
   });
 
-  // Insert image
-  image_input.addEventListener("change", function () {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-        uploaded_image = reader.result;
-        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
-    });
-    reader.readAsDataURL(this.files[0]);
-  })
 });
